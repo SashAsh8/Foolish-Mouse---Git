10884f87738a690be553622ec800eaf55c8c8680
Ash <sasha.polonyiova@gmail.com>
2024-12-20 20:05:04 +0100

    l

--- Foolish Mouse-Git/Foolish Mouse/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Foolish Mouse-Git/Foolish Mouse/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -31,10 +31,10 @@ namespace StarterAssets
         public AudioClip LandingAudioClip;
         public AudioClip[] FootstepAudioClips;
         [Range(0, 1)] public float FootstepAudioVolume = 0.5f;
-
-        [Space(10)]
-        [Tooltip("The height the player can jump")]
-        public float JumpHeight = 1.2f;
+        
+       [Space(10)]
+       [Tooltip("The height the player can jump")]
+       public float JumpHeight = 1.2f;
 
         [Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
         public float Gravity = -15.0f;
